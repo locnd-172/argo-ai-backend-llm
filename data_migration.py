9c246763d14846c13d93adf1eb68e6d6d1274bc5
npt <npt@netcompany.com>
2024-07-05 18:13:08 +0700

    improve writing performance

--- data_migration.py
+++ data_migration.py
@@ -1,6 +1,8 @@
+import time
 from datetime import datetime
 import firebase
 import pandas as pd
+from collections import deque
 
 db = firebase.db
 bulk_writer = db.bulk_writer()
@@ -26,27 +28,40 @@ def create_data_item(row):
 
 def write_data_to_firestore(df):
     facility_docs = {}
+    facility_data = {}
+    facility_exists = {}
+
+    bulk_writer = db.bulk_writer()
 
     for index, row in df.iterrows():
         data_item = create_data_item(row)
         facility = row.get('facility', None)
 
         doc_ref = facility_docs.setdefault(facility, collection.document())
-        doc = doc_ref.get()
-        if doc.exists:
-            data = doc.to_dict()
-            existing_data = data.get('data', [])
-            existing_data.append(data_item)
-            data['data'] = existing_data
+        if facility not in facility_data:
+            if facility not in facility_exists:
+                doc = doc_ref.get()
+                facility_exists[facility] = doc.exists
+            if facility_exists[facility]:
+                data = doc.to_dict()
+                facility_data[facility] = deque(data.get('data', []))
+            else:
+                facility_data[facility] = deque()
+
+        facility_data[facility].append(data_item)
+
+        data = {
+            'facility': facility,
+            'location': {key: row.get(key, None) for key in
+                         ['district', 'latitude', 'address', 'province', 'longitude']},
+            'data': list(facility_data[facility])
+        }
+
+        if facility_exists[facility]:
             bulk_writer.update(doc_ref, data)
         else:
-            data = {
-                'facility': facility,
-                'location': {key: row.get(key, None) for key in
-                             ['district', 'latitude', 'address', 'province', 'longitude']},
-                'data': [data_item]
-            }
             bulk_writer.set(doc_ref, data)
+            facility_exists[facility] = True
 
     bulk_writer.flush()
 
@@ -59,5 +74,8 @@ def delete_all_documents(collection):
 
 if __name__ == '__main__':
     df = pd.read_excel('./data.xlsx', sheet_name='data_temp')
+    start = time.time()
     write_data_to_firestore(df)
+    end = time.time()
+    print(f'Execution time: {end - start} seconds')
     # delete_all_documents(collection)
