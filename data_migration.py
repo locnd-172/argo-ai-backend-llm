e387eb2f1c35e4db529244283145443e820d5b10
npt <npt@netcompany.com>
2024-07-05 16:14:07 +0700

    add data migration

--- data_migration.py
+++ data_migration.py
@@ -1,2 +1,63 @@
+from datetime import datetime
+import firebase
+import pandas as pd
+
+db = firebase.db
+bulk_writer = db.bulk_writer()
+batch = db.batch()
+collection = db.collection('gathering')
+
+
+def create_data_item(row):
+    return {
+        'date': datetime.combine(row['datetime'], datetime.min.time()),
+        'weather': {key: row.get(key, None) for key in
+                    ['humidity', 'rainfall', 'atmospheric_pressure', 'max_temperature', 'cloud', 'wind_direction',
+                     'min_temperature', 'wind']},
+        'pest': {key: row.get(key, None) for key in
+                 ['pest_population_counts', 'disease_incidence', 'severity_of_infestations']},
+        'irrigation': {key: row.get(key, None) for key in
+                       ['water_discharged', 'water_quality', 'water_consumed', 'water_withdrawn', 'water_recycled']},
+        'fertilize': row.get('fertilize', None),
+        'soil': {key: row.get(key, None) for key in
+                 ['soil_nutrient_levels', 'soil_moisture', 'soil_temperature', 'soil_ph']},
+    }
+
+
+def write_data_to_firestore(df):
+    facility_docs = {}
+
+    for index, row in df.iterrows():
+        data_item = create_data_item(row)
+        facility = row.get('facility', None)
+
+        doc_ref = facility_docs.setdefault(facility, collection.document())
+        doc = doc_ref.get()
+        if doc.exists:
+            data = doc.to_dict()
+            existing_data = data.get('data', [])
+            existing_data.append(data_item)
+            data['data'] = existing_data
+            bulk_writer.update(doc_ref, data)
+        else:
+            data = {
+                'facility': facility,
+                'location': {key: row.get(key, None) for key in
+                             ['district', 'latitude', 'address', 'province', 'longitude']},
+                'data': [data_item]
+            }
+            bulk_writer.set(doc_ref, data)
+
+    bulk_writer.flush()
+
+
+def delete_all_documents(collection):
+    docs = collection.list_documents()
+    for doc in docs:
+        doc.delete()
+
+
 if __name__ == '__main__':
-    print("migrating")
+    df = pd.read_excel('./data.xlsx', sheet_name='data_temp')
+    write_data_to_firestore(df)
+    # delete_all_documents(collection)
--- firebase.py
+++ firebase.py
@@ -1,13 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.RefreshToken('./argoai-63051-firebase-adminsdk-ykwbi-a14bbb8c68.json')
+cred = credentials.Certificate('./argoai-63051-firebase-adminsdk-ykwbi-a14bbb8c68.json')
 
-app = firebase_admin.initialize_app()
+app = firebase_admin.initialize_app(cred)
 db = firestore.client()
-
-batch = db.batch()
-
-# Set the data for NYC
-nyc_ref = db.collection("cities").document("NYC")
-batch.set(nyc_ref, {"name": "New York City"})
