56a737b2ee9c9fd6faf7651f2453349becec614d
locnd <locnd.fpt@gmail.com>
2024-07-18 21:48:27 +0700

    [chore] remove mysql

--- src/config/constant.py
+++ src/config/constant.py
@@ -5,13 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class MySQLCFG:
-    MYSQL_HOST = os.environ["MYSQL_HOST"]
-    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "argo_ai")
-    MYSQL_USER = os.getenv("MYSQL_USER", "username")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
-
-
 class GeminiAiCFG:
     API_KEY = os.environ["GEMINI_API_KEY"]
     API_MODEL = os.getenv("GEMINI_API_MODEL", "gemini-pro")
