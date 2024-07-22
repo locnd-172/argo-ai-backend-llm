50424ec0ef83d1b3177ba9c40d81bb2fc98264ac
locnd <locnd.fpt@gmail.com>
2024-07-22 21:12:18 +0700

    [update] improve scouting report

--- src/module/llm/prompts/prompt_scouting.py
+++ src/module/llm/prompts/prompt_scouting.py
@@ -3,13 +3,12 @@ You are professor in agriculture, farming and crops.
 You are an operations monitoring assistant for an agricultural management system that utilizes IoT and sensors.
 You can understand scouting report of a plant from a field and give recommendation for better crops yields as well as performance.
 
-
 # Instruction
 Your task is to understand the reporting metrics related to agricultural farming and scouting reports of a field.
-You have to analyze the message and extract following information: growth, water, pest, disease status.
-Then you will evaluate each metrics condition as good or bad.
+You have to analyze the message and extract following information: growth status, water status, pest status, disease status, and other status.
+Then you will evaluate each metrics condition as "good" or "bad".
 Finally, give your overall evaluation for plant or farming health, and give some suggestion for farmer to optimize current status.
-Respond in Vietnamese.
+Respond with same language as input description.
 
 # Context
 ```
@@ -22,16 +21,19 @@ The description of scouting report is as follows:
 
 You must respond in the following structured JSON format, all fields are mandatory:
 {{
+    "health_evaluation": "overall assessment of farming base on current status, 'good' or 'bad'",
     "assessment": "overall assessment of farming plant or cropping base on provided scouting report description",
     "growth_report": "growth status report of asked plant",
-    "growth_status": "your evaluation on plant growth status. one of following: good, monitor, bad, risk",
+    "growth_status": "your evaluation on plant growth status. one of following: monitor, bad, risk",
+    "soil_report": "soil status report of asked plant/farming/cultivation",
+    "soil_status": "soil status of asked plant/farming/cultivation. one of following: good, bad, neutral",
     "water_report": "water status report of asked plant/farming/cultivation",
-    "water_status": "water status of asked plant/farming/cultivation",
+    "water_status": "water status of asked plant/farming/cultivation. one of following: good, bad, neutral",
     "pest_report": "pest status report of asked plant",
-    "pest_status": "your evaluation on current pest status. one of following: good, bad",
+    "pest_status": "your evaluation on current pest status. one of following: good, bad, neutral",
     "disease_report": "disease status of asked plant/farming",
-    "disease_status": "your evaluation on current disease status. one of following: good, bad",
+    "disease_status": "your evaluation on current disease status. one of following: good, bad, neutral",
     "other_report": "other metrics or additional information report",
-    "suggestion": ""
+    "suggestion": "your recommendation to improve crop yield base on current condition"
 }}
 """
--- src/module/scouting/scouting_services.py
+++ src/module/scouting/scouting_services.py
@@ -1,3 +1,5 @@
+import json
+
 from src.module.llm.gemini.gemini_services import call_model_gemini
 from src.module.llm.prompts.prompt_scouting import PROMPT_PROCESS_SCOUTING_REPORT
 from src.utils.logger import logger
@@ -14,4 +16,37 @@ async def process_scouting_report(data, file):
 
     scouting_report_resp = await call_model_gemini(formatted_prompt)
     logger.info("SCOUTING REPORT: %s", scouting_report_resp)
+
+    scouting_report = {
+        "facility": data.facility,
+        "plant": data.plant,
+        "datetime": data.date,
+        "description": data.description,
+        "health": scouting_report_resp.get("health_evaluation", "neutral"),
+        "assessment": scouting_report_resp.get("assessment", ""),
+        "suggestion": scouting_report_resp.get("suggestion", ""),
+        "growth": {
+            "report": scouting_report_resp.get("growth_report", ""),
+            "status": scouting_report_resp.get("growth_status", "neutral")
+        },
+        "soil": {
+            "report": scouting_report_resp.get("soil_report", ""),
+            "status": scouting_report_resp.get("soil_status", "neutral")
+        },
+        "water": {
+            "report": scouting_report_resp.get("water_report", ""),
+            "status": scouting_report_resp.get("water_status", "neutral")
+        },
+        "pest": {
+            "report": scouting_report_resp.get("pest_report", ""),
+            "status": scouting_report_resp.get("pest_status", "neutral")
+        },
+        "disease": {
+            "report": scouting_report_resp.get("disease_report", ""),
+            "status": scouting_report_resp.get("disease_status", "neutral")
+        },
+        "other_report": scouting_report_resp.get("other_report", ""),
+    }
+    logger.info("SCOUTING REPORT: %s", json.dumps(scouting_report, indent=4, ensure_ascii=False))
+
     return scouting_report_resp
