69aa7cf6a0b27b4e49e3599f6858113353d711ae
locnd <locnd.fpt@gmail.com>
2024-07-27 01:41:19 +0700

    [update] aggregate facility emission data

--- src/module/ghg_emission/ghg_emission_services.py
+++ src/module/ghg_emission/ghg_emission_services.py
@@ -1,13 +1,13 @@
-import json
-import time
 from datetime import datetime
+
+import pandas as pd
 
 from src.config.constant import FirebaseCFG
 from src.module.databases.firebase.firestore import FirestoreWrapper
+from src.module.ghg_emission.ghg_emission_calculator import GHGEmissionCalculator
 from src.module.llm.gemini.gemini_services import call_model_gemini
 from src.module.llm.prompts.prompt_ghg_emission import PROMPT_GHG_EMISSION_PROCESS
 from src.utils.logger import logger
-from src.module.ghg_emission.ghg_emission_calculator import GHGEmissionCalculator
 
 
 async def process_ghg_emission(data):
@@ -59,11 +59,11 @@ def calculate_ghg_emission(
     }
 
 
-async def extract_emission_data(data):
-    firestore = FirestoreWrapper()
+async def fetch_mrv_data(data):
     period_start = datetime.strptime(data.period_start, '%d/%m/%Y')
     period_end = datetime.strptime(data.period_end, '%d/%m/%Y')
-    mrv_data = await firestore.retrieve_data(
+    firestore = FirestoreWrapper()
+    return await firestore.retrieve_data(
         collection_name=FirebaseCFG.FS_COLLECTION_MRV,
         query_filters=[
             ("facility", "==", data.facility),
@@ -73,10 +73,40 @@ async def extract_emission_data(data):
         order_by="date"
     )
 
-    mrv_data = [{"date": datetime.strftime(item["date"], "%d/%m/%Y"), **item} for item in mrv_data]
-    logger.info("mrv_data: %s", mrv_data)
-    emission_data_response = {
-        "length": len(mrv_data),
-        "mrv_data": mrv_data
+
+def aggregate_emission_data(mrv_data):
+    energy_totals = {"gasoline": 0, "diesel": 0, "electricity": 0}
+    water_totals = {"days_flooded": 0, "water_consumed": 0, "water_recycled": 0}
+    irrigation_types = set()
+
+    for item in mrv_data:
+        irrigation_data = item["data"]["irrigation"]
+        energy_data = item["data"]["energy"]
+
+        water_totals["days_flooded"] += irrigation_data["days_flooded"]
+        water_totals["water_consumed"] += irrigation_data["water_consumed"]
+        water_totals["water_recycled"] += irrigation_data["water_recycled"]
+        energy_totals["gasoline"] += energy_data["gasoline"]
+        energy_totals["diesel"] += energy_data["diesel"]
+        energy_totals["electricity"] += energy_data["electricity"]
+
+        irrigation_types.add(irrigation_data["irrigation_type"])
+
+    irrigation_types = list(irrigation_types)
+    irrigation_type = irrigation_types[0] if len(irrigation_types) > 0 else "continuous_flooding"
+
+    return {
+        "energy": energy_totals,
+        "water": water_totals,
+        "irrigation_type": irrigation_type
     }
-    return emission_data_response
+
+
+async def extract_emission_data(data):
+    mrv_data = await fetch_mrv_data(data)
+    logger.info("mrv_data: %s", mrv_data)
+
+    emission_data = aggregate_emission_data(mrv_data)
+    logger.info("emission_data: %s", emission_data)
+
+    return emission_data
